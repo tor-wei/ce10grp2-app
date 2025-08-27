d4f1df02f2a5045d1e063b76995ba0afa829bab8
leoeric <leoeric13@gmail.com>
2025-08-27 23:12:22 +0800

    add lint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,20 +2,7 @@ import globals from "globals";
 
 export default [
   {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.es2021,
-      },
-      ecmaVersion: 2021,
-      sourceType: "module",
-    },
-    rules: {
-      // Add any custom rules here
-    },
-  },
-  {
-    files: ["**/*.jsx", "**/*.tsx"],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -28,6 +15,9 @@ export default [
       react: {
         version: "detect",
       },
+    },
+    rules: {
+      // React/browser specific rules
     },
   },
 ];
